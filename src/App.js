4fdc912c62812a6d5b7c61f88229642bfb6b8221
Kabsoula <amddurontex@hotmail.com>
2021-10-27 01:13:17 +0100

    updated work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import './Style/Style.css';
+
+
+
 
 function App() {
   return (
-    <div>
-    
-    </div>
+  
+  
+  <div>
+  <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+    <h1 className="title-red">Yahya Trabelsi</h1>
+    <br />
+    <img src="/imageInSrc.jpg" alt="logo"/>
+    <br />
+    <img src="/imageInPublic.jpg" alt="another logo" />
+  </div>
+  <video width={320} height={240} controls>
+    <source src="myVideo.mp4" type="video/mp4" />
+  </video>
+</div>
+  
   );
 }
 
