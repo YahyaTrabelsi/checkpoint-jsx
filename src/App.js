8d8372a3cf719923bbe96f63f9744bdaeedb8b88
Kabsoula <amddurontex@hotmail.com>
2021-10-27 12:30:38 +0100

    JSX update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 // import logo from './logo.svg';
 import './App.css';
 import './Style/Style.css';
-
+import MyImage from './MyImage.jpg';
 
 
 
 function App() {
   return (
-  
-  
   <div>
   <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
     <h1 className="title-red">Yahya Trabelsi</h1>
     <br />
-    <img src="/imageInSrc.jpg" alt="logo"/>
+    <img src={MyImage} className="image-edit" alt=""/>
+    <img src="Image.jpg" className="image-edit" alt="" />
     <br />
-    <img src="/imageInPublic.jpg" alt="another logo" />
+    
+    
   </div>
   <video width={320} height={240} controls>
     <source src="myVideo.mp4" type="video/mp4" />
